1348e0fbd25e27362c08ffe8b457b9d81274c9db
Slurm McKenzie <kristian.kissling@textmaschine.com>
2016-06-12 11:39:05 +0000

    add field and test search word

--- logicv2.js
+++ logicv2.js
@@ -3,14 +3,20 @@
 // Alphabet als Array definieren, ohne Umlaute
 var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
+// Search Word
+var searchWord = ['A', 'F', 'F', 'E', 'N']; 
+
 // Number of all fields in the game
 var completeField = 36;
 
 // Laenge des Alphabets auslesen
 var arrayLength = alphabet.length;
 
+//find out search word length
+var searchWordLength = searchWord.length;
+
 // CompleteField consists of random numbers plus searchWord
-var reducedField = completeField - searchWord
+var reducedField = completeField - searchWordLength
 
 // Minimum fuer Zufall festlegen
 var min = 1;
@@ -25,4 +31,6 @@ var max = arrayLength;
 
 
 // Zufaellig erzeugten Buchstaben aus 'alphabet' auswaehlen
-console.log(alphabet[random(min, max)]);
+console.log("Buchstabe aus dem Alphabet:" + alphabet[random(min, max)]);
+console.log("Suchwortlaenge:" + searchWordLength);
+console.log("Feldgroesse:" + reducedField);
