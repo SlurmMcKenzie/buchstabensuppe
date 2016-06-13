b11e10d64c783b05d3c43e4d436a2f2c31a65f85
Slurm McKenzie <kristian.kissling@textmaschine.com>
2016-06-13 07:09:27 +0000

    chose random letters from alphabet according to reducedField

--- logicv2.js
+++ logicv2.js
@@ -4,33 +4,41 @@
 var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 // Search Word
-var searchWord = ['A', 'F', 'F', 'E', 'N']; 
+var searchWord = ['A', 'F', 'F', 'E'];
 
 // Number of all fields in the game
 var completeField = 36;
 
-// Laenge des Alphabets auslesen
-var arrayLength = alphabet.length;
+// Laenge des Alphabets auslesen, wg. Abrunden - 1
+var arrayLength = alphabet.length - 1;
 
 //find out search word length
 var searchWordLength = searchWord.length;
 
 // CompleteField consists of random numbers plus searchWord
-var reducedField = completeField - searchWordLength
+var reducedField = completeField - searchWordLength;
 
 // Minimum fuer Zufall festlegen
-var min = 1;
+var min = 0;
 
 // Maximum fuer Zufall = Laenge des Alphabets
 var max = arrayLength;
 
-	// Zufaelligen Zufall generieren, Zahl zwischen 1 und 26.
-	function random (min , max) {
-		return Math.floor(Math.random() * (max - min + 1)) + min;
-	}
+// Text in For-Schleife
+var text = [];
 
+// Zufaelligen Zufall generieren, Zahl zwischen 0 und 25.
+function random(min, max) {
+    "use strict";
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+for (i = 1 ; i <= reducedField; i++) {
+    text += alphabet[random(min, max)];
+}
 
 // Zufaellig erzeugten Buchstaben aus 'alphabet' auswaehlen
-console.log("Buchstabe aus dem Alphabet:" + alphabet[random(min, max)]);
-console.log("Suchwortlaenge:" + searchWordLength);
-console.log("Feldgroesse:" + reducedField);
+console.log(text);
+//console.log("Buchstabe aus dem Alphabet:" + alphabet[random(min, max)]);
+//console.log("Suchwortlaenge:" + searchWordLength);
+//console.log("Feldgroesse:" + reducedField);
